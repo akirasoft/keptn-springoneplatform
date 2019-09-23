b2445ec9595d7158dfbc7ebc1592e7dec6f96fcc
akirasoft <akirasoft@gmail.com>
2019-09-23 12:47:35 -0500

    changes to hub delete

--- docker-image/delete-repo.sh
+++ docker-image/delete-repo.sh
@@ -3,4 +3,4 @@ export GITHUB_USER=$(cat creds.json | jq -r '."githubUserName"')
 export GITHUB_TOKEN=$(cat creds.json | jq -r '."githubPersonalAccessToken"')
 export GITHUB_ORG=$(cat creds.json | jq -r '."githubOrg"')
 export GITHUB_REPO="sockshop"
-/usr/bin/hub delete $GITHUB_ORG/$GITHUB_REPO
+hub delete $GITHUB_ORG/$GITHUB_REPO
